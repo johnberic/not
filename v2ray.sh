841b254b291b04291ae0dc0c1522a9b85dba5b65
tknetwork <154647403+johnberic@users.noreply.github.com>
2025-04-22 22:51:56 -0700

    Update v2ray.sh

--- v2ray.sh
+++ v2ray.sh
@@ -2,7 +2,7 @@
 
 # Generate JSON array for client credentials
 #!/bin/bash
-if ! diff -q /etc/authorization/pandavpnunite/uuid.sh /etc/authorization/pandavpnunite/prev_uuid.sh >/dev/null; then
+if ! diff -q /home/active.sh /home/active.sh >/dev/null; then
     echo "Active not match."
     client_array="["
     while IFS= read -r uuid; do
@@ -11,7 +11,7 @@ if ! diff -q /etc/authorization/pandavpnunite/uuid.sh /etc/authorization/pandavp
 
         # Append the uuid
         client_array+="{\"id\": \"$uuid\", \"alterId\": 64}, "
-    done < /etc/authorization/pandavpnunite/uuid.sh
+    done < /home/active.sh
     client_array="${client_array%, }]"  # Remove trailing comma and add closing bracket
 
     # Update V2Ray configuration file with the client array
@@ -19,7 +19,7 @@ if ! diff -q /etc/authorization/pandavpnunite/uuid.sh /etc/authorization/pandavp
     #echo $client_array
     cp /usr/local/etc/v2ray/default-config.json /usr/local/etc/v2ray/config.json
     sed -i "s/\"clients\": \[\]/\"clients\": $client_array/" /usr/local/etc/v2ray/config.json
-    cp /etc/authorization/pandavpnunite/uuid.sh /etc/authorization/pandavpnunite/prev_uuid.sh
+    cp /home/active.sh /home/active.sh
     sudo systemctl restart v2ray
 
 fi
